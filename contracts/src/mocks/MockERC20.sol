7960b8f78d7feb9c5318524561eb6330c7971751
Md Nazmul Hasan <farhapartex@gmail.com>
2026-08-09 19:23:20 +0600

    (feat): setup foundry project and add mock erc20 token

---
+++ contracts/src/mocks/MockERC20.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    uint8 private immutable tokenDecimals;
+
+    constructor(string memory tokenName, string memory tokenSymbol, uint8 tokenDecimalPlaces)
+        ERC20(tokenName, tokenSymbol)
+    {
+        tokenDecimals = tokenDecimalPlaces;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return tokenDecimals;
+    }
+
+    function mint(address receiver, uint256 amount) external {
+        _mint(receiver, amount);
+    }
+
+    function burn(address holder, uint256 amount) external {
+        _burn(holder, amount);
+    }
+}
